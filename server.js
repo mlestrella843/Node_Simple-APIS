28128c30708ff5b4ea9158f62a778ca464fb334e
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-24 05:58:26 -0600

    Installing dependencies dev

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require( 'express' );
 const app = express();
 app.set( 'port', process.env.PORT || 9000 );
 
+app.get('/', (req,res) => {
+    res.send('Welcome to my Simple API Project');
+})
+
 app.listen( app.get('port'), () => {
     console.log( ' Server running on port', app.get('port') );
 })
