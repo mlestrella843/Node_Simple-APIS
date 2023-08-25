076afd7a9fb1f531b2f17c72640ac321be46f753
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-24 20:19:30 -0600

    Conifuring nodemon, mysql, myconnection

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require( 'express' );
+const mysql = require( 'mysql' );
+const myconn = require( 'express-myconnection');
 
+
+//initializing and configurations-----------------------------------
 const app = express();
 app.set( 'port', process.env.PORT || 9000 );
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'abc123',
+    database: library
+}
 
+//midlewares
+app.use( myconn(mysql, dbOptions, 'single'));
+
+
+
+//routes------------------------------------------------------------
 app.get('/', (req,res) => {
     res.send('Welcome to my Simple API Project');
 })
 
+
+//SERVER runing-----------------------------------------------------
 app.listen( app.get('port'), () => {
     console.log( ' Server running on port', app.get('port') );
 })
