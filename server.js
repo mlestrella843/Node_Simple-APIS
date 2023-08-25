1c95ef50584c39a923f36720b2630fa254864bcd
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-25 16:57:55 -0600

    Testing new routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require( 'express' );
-const mysql = require( 'mysql' );
-const myconn = require( 'express-myconnection');
+const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
 
 
 //initializing and configurations-----------------------------------
@@ -10,18 +10,20 @@ const dbOptions = {
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'abc123',
-    database: library
+    password: 'abc_1234',
+    database: 'library'
 }
 
 //midlewares
-app.use( myconn(mysql, dbOptions, 'single'));
-
-
+ app.use( myconn(mysql, dbOptions, 'single'))
 
 //routes------------------------------------------------------------
 app.get('/', (req,res) => {
     res.send('Welcome to my Simple API Project');
+})
+
+app.get('/api', (req, res) => {
+    res.send( ' Probando nueva api /api');
 })
 
 
